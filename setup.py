ecb518270004d1a9965f7506fe9d9fc08b92230f
Owais <owais.mahmudi@gmail.com>
2013-01-23 11:49:23 +0100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+name='Owais Mahmudi',
+version='0.1',
+author='owaism',
+author_email='owais.mahmudi@gmail.com',
+url='https://github.com/owaismahmudi',
+packages=['owaism',],
+license='GPLv3',
+long_description=open('README.txt').read(),
+)
