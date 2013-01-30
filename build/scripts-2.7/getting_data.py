0bb01c8e7f49297978ad3d685a993dcffe2020f0
owaismahmudi <owais.mahmudi@gmail.com>
2013-01-30 13:51:59 +0100

    import statement corrected

--- build/scripts-2.7/getting_data.py
+++ build/scripts-2.7/getting_data.py
@@ -1,5 +1,5 @@
 #!/Users/mahmudi/.virtualenvs/py2.7/bin/python
 
-from mahmudi import getting_datai
+from mahmudi.getData import getting_datai
 
 getting_datai()
--- scripts/getting_data.py
+++ scripts/getting_data.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
-from mahmudi import getData
+from mahmudi.getData import getting_datai
 
 getting_datai()
