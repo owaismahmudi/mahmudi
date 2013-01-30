1210b2a57d89a3d76c8114ac89a05ba3709743fe
owaismahmudi <owais.mahmudi@gmail.com>
2013-01-30 12:57:18 +0100

    personal experimentation

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
+from setuptools import setup, find_packages
+import sys, os
 from distutils.core import setup
+
 setup(
 name='Owais Mahmudi',
 version='0.1',
 author='owaism',
 author_email='owais.mahmudi@gmail.com',
 url='https://github.com/owaismahmudi',
-packages=['owaism',],
+packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+scripts=['scripts/getting_data.py'],
 license='GPLv3',
 long_description=open('README.txt').read(),
 )
